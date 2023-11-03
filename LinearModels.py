a141840a5a3a4e10c89a9ce999d15df9afa821e8
Prach Techameena <prach.techameena@ki.se>
2023-11-03 15:10:45 +0100

    First commit

---
+++ LinearModels.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, lr:float = 0.01, max_iter:int = 1000):
+        """Linear Regression Model using Gradient Descent
+
+        Args:
+            lr (float, optional): Parameter for Learning-rate. Defaults to 0.01.
+            max_iter (int, optional): Maximum iteration for the model to learn. Defaults to 1000.
+        """
+        self.max_iter = max_iter
+        self.lr = lr
+        self.weights = None
+        self.bias = None
+    
+    def predict(self, X:np.ndarray):
+        # y = wX + b
+        return np.dot(X, self.weights) + self.bias
+        
+    def fit(self, X:np.ndarray, y:np.ndarray):
+        obs, vars = X.shape
+
+        # Initialize Weights and Bias
+        self.weights = np.zeros(vars)
+        self.bias = 0
+        
+        # iterate over max_iter
+        for i in range(self.max_iter):
+            y_pred = self.predict(X)
+            
+            # Compute Gradient Descent of Weights and Bias
+            dw = (1/obs) * np.dot(X.T, (y_pred - y))
+            db = (1/obs) * np.sum(y_pred - y)
+            
+            # Update Weights and Bias
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+            
+
+class LogisticRegression:
+    def __init__(self, lr:float = 0.01, max_iter:int = 1000, threshold:float = 0.5):
+        """Logistic Sigmoid Regression Model using Gradient Descent
+
+        Args:
+            lr (float, optional): Parameter for Learning-rate. Defaults to 0.01.
+            max_iter (int, optional): Maximum iteration for the model to learn. Defaults to 1000.
+            threshold (float, optional): Threshold for the model to classify. This should range from 0 to 1. Defaults to 0.5.
+        """
+        self.max_iter = max_iter
+        self.lr = lr
+        self.threshold = threshold
+        self.weights = None
+        self.bias = None
+        
+    def _sigmoid(self, z:np.ndarray):
+        return 1 / (1 + np.exp(-z))
+    
+    def predict(self, X:np.ndarray):
+        # y = sigmoid(wX + b)
+        return (self._sigmoid(np.dot(X, self.weights) + self.bias) > self.threshold).astype(int)
+    
+    def fit(self, X:np.ndarray, y:np.ndarray):
+        obs, vars = X.shape
+        self.weights = np.zeros(vars)
+        self.bias = 0
+        
+        for i in range(self.max_iter):
+            y_pred = self.predict(X)
+            
+            # Compute Gradient Descent of Weights and Bias
+            dw = (1/obs) * np.dot(X.T, (y_pred - y))
+            db = (1/obs) * np.sum(y_pred - y)
+            
+            # Update Weights and Bias
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+        
